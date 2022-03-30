2bbe543cec7e39fce7b5b4ef48b15415ab05f201
LambdaUser <LambdaEmail>
2022-03-30 04:33:18 -0600

    Added Models Tests, renamed some classes and deleted aws json table

--- project_backend/src/main/java/com/bloomshoppingcomplex/EndPoint/GetStore.java
+++ project_backend/src/main/java/com/bloomshoppingcomplex/EndPoint/GetStore.java
@@ -7,15 +7,15 @@ import com.bloomshoppingcomplex.DynamoDB.Models.Store;
 import com.bloomshoppingcomplex.DynamoDB.StoreDao;
 import com.bloomshoppingcomplex.Exceptions.StoreNotFoundException;
 import com.bloomshoppingcomplex.Models.StoreModel;
-import com.bloomshoppingcomplex.Models.Request.GetStoreInfoRequest;
-import com.bloomshoppingcomplex.Models.result.GetStoreInfoResult;
+import com.bloomshoppingcomplex.Models.Request.GetStoreRequest;
+import com.bloomshoppingcomplex.Models.result.GetStoreResult;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 
-public class GetStoreInfo implements RequestHandler<GetStoreInfoRequest, GetStoreInfoResult> {
+public class GetStoreInfo implements RequestHandler<GetStoreRequest, GetStoreResult> {
     private final Logger log = LogManager.getLogger();
     private final StoreDao storeDao;
 
@@ -25,18 +25,19 @@ public class GetStoreInfo implements RequestHandler<GetStoreInfoRequest, GetStor
     }
 
     @Override
-    public GetStoreInfoResult handleRequest(final GetStoreInfoRequest getStoreInfoRequest, Context context) {
-        log.info("Received GetStoreInfoRequest{}", getStoreInfoRequest);
-        String requestedStoreId = getStoreInfoRequest.getStoreId();
+    public GetStoreResult handleRequest(final GetStoreRequest getStoreRequest, Context context) {
+        log.info("Received GetStoreInfoRequest{}", getStoreRequest);
+        String requestedStoreId = getStoreRequest.getStoreId();
 
-        if (requestedStoreId == null) {
+        Store store = storeDao.getStore(requestedStoreId);
+        if (store == null) {
             throw new StoreNotFoundException(requestedStoreId + " Store ID not found.");
         }
 
-        Store store = storeDao.getStore(requestedStoreId);
+
         StoreModel storeModel = new ModelConverter().toStoreModel(store);
 
-        return GetStoreInfoResult.builder()
+        return GetStoreResult.builder()
                 .withStore(storeModel)
                 .build();
     }
